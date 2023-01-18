9406c109db384b2b85aff4d9ce68319bc358a99d
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-01-18 09:27:35 -0300

    proximo comit

--- index.php
+++ index.php
@@ -28,15 +28,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
     <title>Login Admin</title>
 </head>
 <body>
     <?php
         if(isset($message)){
             foreach($message as $message){
-                echo'
+                echo '
                     <div class="message">
-                        <span>'.$message'</span>
+                        <span>'.$message.'</span>
                         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
                     </div>
                 ';
