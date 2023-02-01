11e074fc70816531a7a74c0b3adbdc6216fd4831
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-01-31 23:49:00 -0300

    proximo comit

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,1 +1,7 @@
-
+<?php
+    if(isset($message)){
+        foreach($message as $message){
+            echo '<div class="message"><span>'.$message.'</span><i class="fas fa-times" onclick="this.parentElemnt.remove();"></i></div>';
+        }
+    }
+?>
