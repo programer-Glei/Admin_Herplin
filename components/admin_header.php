f279dfe78455e6348e3fd1f2e14276a69c579e76
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-02-01 00:30:37 -0300

    proximo comit

--- components/admin_header.php
+++ components/admin_header.php
@@ -5,3 +5,15 @@
         }
     }
 ?>
+
+<header class="header">
+    <section class="flex">
+        <a href="dashboard.php" class="logo">Painel <span>Administrativo</span></a>
+        <nav class="navbar">
+            <a href="dashboard.php">Home</a>
+            <a href="products.php">Produtos</a>
+            <a href="placed_orders.php">Pedidos</a>
+            <a href="admin_accounts.php">Admins</a>
+        </nav>
+    </section>
+</header>
