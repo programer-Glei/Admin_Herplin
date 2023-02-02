460b9d580fccb42d9e782075dfff4241302bb7d6
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-02-02 10:34:43 -0300

    proximo comit

--- components/admin_header.php
+++ components/admin_header.php
@@ -27,6 +27,13 @@
                 $select_profile->execute([$admin_id]);
                 $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
             ?>
+            <p><?=$fetch_profile['name'];?></p>
+            <a href="update_profile.php" class="btn">Atualizar Perfil</a>
+            <div class="flex-btn">
+                <a href="admin_login.php" class="option-btn">Login</a>
+                <a href="register_admin.php" class="option-btn">Registrar</a>
+            </div>
+            <a href="admin_logout.php" onclick="return confirm('Sair deste site');" class="delete-btn">Sair</a>
         </div>
     </section>
 </header>
