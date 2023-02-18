86697d7160ee831462c525d9174aa6c204b78a1b
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-02-18 20:52:02 -0300

    proximo comit

--- products.php
+++ products.php
@@ -1,4 +1,5 @@
 <?php
+    header('Content-Type: text/html; charset=utf-8');
     include 'components/connect.php';
 
     session_start();
@@ -57,3 +58,28 @@
         header('location:products.php');
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Produtos</title>
+</head>
+<body>
+    <?php include 'components/admin_header.php' ?>
+
+    <!-- add products section starts -->
+    <section class="add-products">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <h3>Adicionar produto</h3>
+        </form>
+    </section>
+</body>
+</html>
