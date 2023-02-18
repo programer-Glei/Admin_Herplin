7b244510e67e4f7bdab18009a07575f05aca5bfe
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-02-18 20:42:47 -0300

    proximo comit

--- products.php
+++ products.php
@@ -26,5 +26,34 @@
 
         $select_products = $conn->prepare("SELECT * FROM `products` WHERE name = ?");
         $select_products->execute([$name]);
+
+        if($select_products->rowCount() > 0){
+            $message[] = 'Produto já existe!';
+        }else{
+            if($image_size > 2000000){
+                $message[] = 'O tamanho da imagem é muito grande';
+            }else{
+                move_uploaded_file($image_tmp_name,$image_folder);
+
+                $insert_product = $conn->prepare("INSERT INTO `products`(name, category, price, image) VALUES(?,?,?,?)");
+                $insert_product->execute([$name,$category,$price,$image]);
+
+                $message[] = 'Novo produto adicionado!';
+            }
+        }
+    }
+
+    if(isset($_GET['delete'])){
+
+        $delete_id = $_GET['delete'];
+        $delete_product_image = $conn->prepare("SELECT * FROM `products` WHERE id = ?");
+        $delete_product_image->execute([$delete_id]);
+        $fetch_delete_image = $delete_product_image->fetch(PDO::FETCH_ASSOC);
+        unlink('uploaded_img').$fetch_delete_image['image'];
+        $delete_product = $conn->prepare("DELETE FROM `products` WHERE id = ?");
+        $delete_product->execute([$delete_id]);
+        $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE pid = ?");
+        $delete_cart->execute([$delete_id]);
+        header('location:products.php');
     }
 ?>
