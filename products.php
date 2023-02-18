fa06d735711491ca56d026897ca7735b880f30ec
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-02-17 21:41:14 -0300

    proximo comit

--- products.php
+++ products.php
@@ -1,1 +1,19 @@
+<?php
+    include 'components/connect.php';
 
+    session_start();
+
+    $admin_id = $_SESSION['admin_id'];
+
+    if(!isset($admin_id)){
+        header('location:index.php');
+    };
+
+    if(isset($_POST['add_product'])){
+
+        $name = $_POST['name'];
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+        $price = $_POST['price'];
+        $price = filter_var($price, FILTER_SANITIZE_STRING);
+    }
+?>
