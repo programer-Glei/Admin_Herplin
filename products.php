663a2d2ec45301bc5a7342f61da45586e01b9784
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-02-18 20:18:20 -0300

    proximo comit

--- products.php
+++ products.php
@@ -15,5 +15,16 @@
         $name = filter_var($name, FILTER_SANITIZE_STRING);
         $price = $_POST['price'];
         $price = filter_var($price, FILTER_SANITIZE_STRING);
+        $category = $_POST['category'];
+        $category = filter_var($category, FILTER_SANITIZE_STRING);
+
+        $image = $_FILES['image']['name'];
+        $image = filter_var($image, FILTER_SANITIZE_STRING);
+        $image_size = $_FILES['image']['size'];
+        $image_tmp_name = $_FILES['image']['tmp_name'];
+        $image_folder = 'uploaded_img'.$image;
+
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE name = ?");
+        $select_products->execute([$name]);
     }
 ?>
