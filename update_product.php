6ddb97eb9d6b7b2e29bb7a111b86c9ba8770950a
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-03-05 22:01:49 -0300

    proximo comit

--- update_product.php
+++ update_product.php
@@ -36,7 +36,30 @@
         if(!empty($image)){
             if($image_size > 2000000){
                 $message[] = 'imagem muito grande!';
+            }else{
+                $update_image = $conn->prepare("UPDATE `products` SET image = ? WHERE id = ?");
+                $update_image->execute([$image, $pid]);
+                move_uploaded_file($image_tmp_name, $image_folder);
+                unlink('uplaoded_img'.$old_image);
+                $message[] = 'imagem atualizada!';
             }
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Atualizar Produtos</title>
+</head>
+<body>
+    
+</body>
+</html>
