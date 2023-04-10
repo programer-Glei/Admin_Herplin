7d65fcd826866b07d6c85f16eedbbe261206be05
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-04-10 10:09:17 -0300

    proximo comit

--- placed_orders.php
+++ placed_orders.php
@@ -21,6 +21,9 @@
 
     if(isset($_GET['delete'])){
         $delete_id = $_GET['delete'];
+        $delete_order = $conn->("DELETE FROM `orders` WHERE id = ?");
+        $delete_order->execute([$delete_id]);
+        header('location:placed_orders.php');
     }
 ?>
 
@@ -30,9 +33,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Pedidos</title>
 </head>
 <body>
-    
+    <?php include 'components/admin_header.php' ?>
+
+    <!--  placed orders section starts  -->
+    <section class="placed-orders">
+        <h1 class="heading">Pedidos Feitos</h1>
+        <div class="box-container">
+            <?php
+                $select_orders = $conn->prepare("SELECT * FROM `orders`");
+                $select_orders->execute();
+                if($select_orders->rowCount() > 0){
+                    while($fecth_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
+
+                    }
+                }else{
+                    echo '<p class="empty">Nenhum pedido feito ainda!</p>';
+                }
+            ?>
+
+            <div class="box">
+                <p>ID do usuário: <span><?= $fecth_orders['user_id'];?></span></p>
+                <p>Data: <span><?= $fecth_orders['placed_on'];?></span></p>
+                <p>Nome: <span><?= $fecth_orders['name'];?></span></p>
+                <p>Email: <span><?= $fecth_orders['email'];?></span></p>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
