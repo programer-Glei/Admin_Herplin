322da79f19318f96633da9feb4f666639d590de4
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-04-14 11:40:00 -0300

    proximo comit

--- messages.php
+++ messages.php
@@ -11,6 +11,9 @@
 
     if(isset($_GET['delete'])){
         $delete_id = $_GET['delete'];
+        $delete_message = $conn->prepare("DELETE FROM `messages` WHERE id = ?");
+        $delete_message->execute($delete_id);
+        header('messages.php');
     }
 ?>
 
@@ -20,9 +23,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Mensagens</title>
 </head>
 <body>
-    
+    <?php include 'components/admin_header.php'  ?>
+
+    <!-- messages section starts  -->
+    <section class="messages">
+        <h1 class="heading">Mensagens</h1>
+        <div class="box-container">
+            <?php
+                $select_messages = $conn->prepare("SELECT * FROM `messages`");
+                $select_messages->execute();
+                if($select_messages->rowCount() > 0){
+                    while($fetch_messages = $select_messages->fetch(PDO::FETCH_ASSOC))
+                }else{
+
+                }
+            ?>
+            <div class="box">
+                <p>Nome: <span></span></p>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
