12b550ed4a8d0a52eccf0de67364ad5b4927307e
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-04-14 11:29:50 -0300

    proximo comit

--- messages.php
+++ messages.php
@@ -1,1 +1,28 @@
+<?php
+    include 'components/connect.php';
 
+    session_start();
+
+    $admin_id = $_SESSION['admin_id'];
+
+    if(!isset($admin_id)){
+        header('location:index.php');
+    }
+
+    if(isset($_GET['delete'])){
+        $delete_id = $_GET['delete'];
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
